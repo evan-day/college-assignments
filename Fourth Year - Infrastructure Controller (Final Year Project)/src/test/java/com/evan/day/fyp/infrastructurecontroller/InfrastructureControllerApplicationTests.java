f3ba358093f7bd45a786ce8cbe97f955ec6eaa8d
Evan Day <evan@evanday.dev>
2020-01-11 11:41:29 +0000

    added infrastructure controller

---
+++ Fourth Year - Infrastructure Controller (Final Year Project)/src/test/java/com/evan/day/fyp/infrastructurecontroller/InfrastructureControllerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.evan.day.fyp.infrastructurecontroller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InfrastructureControllerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
